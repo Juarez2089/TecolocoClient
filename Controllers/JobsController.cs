c6afcdd3cc9342c29e8813581e78c5acd14d1490
m j <mmejia2089@hotmail.com>
2020-10-19 21:34:35 -0600

    AddingReadJobs

---
+++ Controllers/JobsController.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using TecolocoClient.Models;
+
+namespace TecolocoClient.Controllers
+{
+    public class JobsController : Controller
+    {
+        // GET: Jobs
+        public ActionResult Index()
+        {
+            HttpClient clientHttp = new HttpClient();
+            clientHttp.BaseAddress = new Uri("https://localhost:44398/");
+            var request = clientHttp.GetAsync("api/Jobs").Result;
+
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var list = JsonConvert.DeserializeObject<List<Jobs>>(resultString);
+                return View(list);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Models/Jobs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TecolocoClient.Models
+{
+    public class Jobs
+    {
+        [Key]
+        public string JobID { get; set; }
+        [Required(ErrorMessage = "Ingrese un job de al menos 3 caracteres."), MinLength(3)]
+        public string JobName { get; set; }
+        [Required(ErrorMessage = "Ingrese un titulo de al menos 3 caracteres"), MinLength(3)]
+        public string JobTitle { get; set; }
+        [Required(ErrorMessage = "Ingrese una descripción de al menos 20 caracteres."), MinLength(20)]
+        public string Description { get; set; }
+        public DateTime CreateAt { get; set; }
+        [Required(ErrorMessage = "Ingrese una fecha válida."), DataType(DataType.Date)]
+        public DateTime ExpiresAt { get; set; }
+
+    }
+}
